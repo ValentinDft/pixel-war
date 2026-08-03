316c7a70ccea595d3101ff22c95ef5b70a3ab6d3
ValentinDft <vavadu07@gmail.com>
2026-08-03 21:26:14 +0200

    feat: add header

---
+++ src/app/features/components/header/header.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pix-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {}
